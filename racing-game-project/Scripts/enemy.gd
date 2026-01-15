9ebc9cc068fa515a254a4f60a7e5afcf0134ae19
SoulPixelIV <william.dj@outlook.de>
2026-01-15 18:14:07 +0100

    Worked on enemy AI

---
+++ racing-game-project/Scripts/enemy.gd
@@ -0,0 +1,50 @@
+extends VehicleBody3D
+
+@export var MAX_STEER = 0.35
+@export var ENGINE_POWER = 1600
+@export var MAX_SPEED = 11.5
+@export var ENGINE_BRAKE = 65.0
+
+@export var waypoints: Array[Node3D]
+@export var reach_distance := 3.0
+
+@onready var wheel_fl: VehicleWheel3D = $VehicleWheel3DFL
+@onready var wheel_fr: VehicleWheel3D = $VehicleWheel3DFR
+@onready var wheel_bl: VehicleWheel3D = $VehicleWheel3DBL
+@onready var wheel_br: VehicleWheel3D = $VehicleWheel3DBR
+
+var grounded = false
+var current_wp := 0
+
+func _physics_process(delta: float) -> void:
+	var speed := linear_velocity.length()
+		
+	#Check Grounded
+	if wheel_fl.is_in_contact() and wheel_fr.is_in_contact() and wheel_bl.is_in_contact() and wheel_br.is_in_contact():
+		grounded = true
+	else:
+		grounded = false
+		
+	#Waypoint
+	var target = waypoints[current_wp].global_position
+	var distance_to_target = target - global_position
+	
+	#Engine Force
+	if distance_to_target.length() > 3 && speed < MAX_SPEED:
+		engine_force = ENGINE_POWER
+		
+	#Steering
+	var to_target = (target - global_position)
+	to_target.y = 0
+	to_target = to_target.normalized()
+	
+	var forward = -global_transform.basis.z
+	forward.y = 0
+	forward = forward.normalized()
+	
+	var steer_amount = -forward.cross(to_target).y
+	steering = move_toward(steering, steer_amount * MAX_STEER, delta * 5)
+	
+	#Switch Waypoint
+	if distance_to_target.length() < reach_distance:
+		current_wp = (current_wp + 1) % waypoints.size()
